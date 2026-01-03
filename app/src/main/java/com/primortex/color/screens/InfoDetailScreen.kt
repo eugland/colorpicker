049ea2c4f29dd67e8c490a37e17d49c954545fe9
Eugene Wang <eugene.r.w.12@gmail.com>
2026-01-03 04:30:11 -0800

    privacy and copyright

--- app/src/main/java/com/primortex/color/screens/InfoDetailScreen.kt
+++ app/src/main/java/com/primortex/color/screens/InfoDetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InfoDetailScreen(
     title: String,
