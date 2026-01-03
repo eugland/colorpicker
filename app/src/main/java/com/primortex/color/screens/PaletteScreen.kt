f690620410720a66fbc87a189603d0b6bc19942a
Eugene Wang <eugene.r.w.12@gmail.com>
2026-01-03 15:06:39 -0800

    dependency injected

--- app/src/main/java/com/primortex/color/screens/PaletteScreen.kt
+++ app/src/main/java/com/primortex/color/screens/PaletteScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
