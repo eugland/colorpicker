c002311074655ad22e60e4cd9303062ba01200e5
Eugene Wang <eugene.r.w.12@gmail.com>
2026-01-14 03:01:16 -0800

    products

--- app/src/main/java/com/primortex/color/ui/components/SwatchSection.kt
+++ app/src/main/java/com/primortex/color/ui/components/SwatchSection.kt
@@ -11,10 +11,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
