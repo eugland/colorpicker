17044f1c1922bbe89b6f152f6345e30d0c95fb85
Eugene Wang <eugene.r.w.12@gmail.com>
2026-01-16 02:29:13 -0800

    better tonal color

--- app/src/main/java/com/primortex/color/screens/ExploreScreen.kt
+++ app/src/main/java/com/primortex/color/screens/ExploreScreen.kt
@@ -3,6 +3,7 @@ package com.primortex.color.screens
 import android.content.Context
 import android.content.Intent
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowForwardIos
@@ -223,7 +223,7 @@ private fun StandardCard(
     Surface(
         modifier = Modifier.fillMaxWidth(),
         shape = MaterialTheme.shapes.large,
-        tonalElevation = 2.dp
+        tonalElevation = 1.dp
     ) {
         Column {
             if (title != null) {
