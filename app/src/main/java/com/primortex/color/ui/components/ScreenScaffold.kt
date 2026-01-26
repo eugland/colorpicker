e12537d80d84028e8ec6032524de07ee7cdb9593
Eugene Wang <eugene.r.w.12@gmail.com>
2026-01-25 19:55:20 -0800

    all routes and colors

--- app/src/main/java/com/primortex/color/ui/components/ScreenScaffold.kt
+++ app/src/main/java/com/primortex/color/ui/components/ScreenScaffold.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Icon
